6928d168f04661781cf5dedc963f923b34fc7f7e
Taylor Roberts <tstarrroberts@gmail.com>
2022-07-09 01:51:40 -0400

    add client side and concurrent server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 // create new Apollo server and pass in schema data
@@ -24,6 +25,16 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate apollo server with express as middleware
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
